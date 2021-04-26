a0785afc76cca53ccbf3338eb4dd7da9d7978c3c
Efaz Muhaimen <eem_680@usc.edu>
2021-04-26 14:31:09 -0700

    Modified for Jetson

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 setup(
-  name = 'rpi_python_drv8825',
-  packages = ['rpi_python_drv8825'],
+  name = 'rpi_python_drv8834',
+  packages = ['rpi_python_drv8834'],
   version = '0.3',
   license='gpl-3.0',
-  description = 'Python library to controll a stepper motor with a DRV8825 driver connected to a Raspberry Pi',
+  description = 'Python library to controll a stepper motor with a DRV8834 driver connected to a Raspberry Pi',
   author = 'Lukas Schmid',
   author_email = 'dimschlukas@gmail.com',
-  url = 'https://github.com/dimschlukas/rpi_python_drv8825',
-  download_url = 'https://github.com/dimschlukas/rpi_python_drv8825/archive/v0.3-alpha.tar.gz',
-  keywords = ['drv8825', 'control', 'steppermotor', 'stepper', 'motor', 'Raspberry', 'Pi'],
+  url = 'https://github.com/dimschlukas/rpi_python_drv8834',
+  download_url = 'https://github.com/dimschlukas/rpi_python_drv8834/archive/v0.3-alpha.tar.gz',
+  keywords = ['drv8834', 'control', 'steppermotor', 'stepper', 'motor', 'Raspberry', 'Pi'],
   install_requires=[
-          'RPi.GPIO',
+          'Jetson.GPIO',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
