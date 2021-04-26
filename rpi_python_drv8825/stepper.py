d96743a161efe374e6d8912f0289f426456243db
Efaz Muhaimen <eem_680@usc.edu>
2021-04-26 13:34:36 -0700

    Modified for DRV8834

--- rpi_python_drv8825/stepper.py
+++ rpi_python_drv8825/stepper.py
@@ -2,23 +2,21 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 class StepperMotor:
-    def __init__(self, enable_pin, step_pin, dir_pin, mode_pins, step_type, fullstep_delay):
+    def __init__(self, step_pin, dir_pin, mode_pins, step_type):
         """docstring for ."""
-        self.enable_pin = enable_pin
         self.step_pin = step_pin
         self.dir_pin = dir_pin
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(enable_pin, GPIO.OUT)
         GPIO.setup(step_pin, GPIO.OUT)
         GPIO.setup(dir_pin, GPIO.OUT)
         GPIO.setup(mode_pins, GPIO.OUT)
-        resolution = {'Full':(0, 0, 0),
-                    'Half':(1, 0, 0),
-                    '1/4':(0, 1, 0),
-                    '1/8':(1, 1, 0),
-                    '1/16':(0, 0, 1),
-                    '1/32':(1, 0, 1)}
+        resolution = {'Full':(0, 0),
+                    'Half':(1, 0),
+                    '1/4':(0, 0), # M0 must be disconnected
+                    '1/8':(0, 1),
+                    '1/16':(1, 1),
+                    '1/32':(0, 1)} # M0 must be disconnected
         microsteps =  {'Full':1,
                     'Half':2,
                     '1/4':4,
@@ -27,9 +25,6 @@ class StepperMotor:
                     '1/32':32}
         self.delay = .005/microsteps[step_type]
         GPIO.output(mode_pins, resolution[step_type])
-
-    def enable(self, enable):
-        GPIO.output(self.enable_pin, not enable)
 
     def run(self, steps, clockwise):
         GPIO.output(self.dir_pin, clockwise)
